7ce04614cce1338f02995e72f41fe83bd615f021
Michael Yang <themichaelyang@gmail.com>
2015-01-22 18:51:09 -0500

    fixed node-webkit/nwjs functionality

--- src/arm.js
+++ src/arm.js
@@ -1,6 +1,22 @@
 var five = require('johnny-five');
 var Leap = require('leapjs');
 
+// fix for node-webkit
+// https://github.com/rwaldron/johnny-five/wiki/Getting-started-with-Johnny-Five-and-Node-Webkit
+var Readable = require("stream").Readable;
+var util = require("util");
+util.inherits(MyStream, Readable);
+function MyStream(opt) {
+  Readable.call(this, opt);
+}
+MyStream.prototype._read = function() {};
+// hook in our stream
+process.__defineGetter__("stdin", function() {
+  if (process.__stdin) return process.__stdin;
+  process.__stdin = new MyStream();
+  return process.__stdin;
+});
+
 var Arm = function(board) {
 	this.board = board;
 
