a0d1b80bb5ee921f8d696bd4b18962450597cb13
Michael Yang <themichaelyang@gmail.com>
2015-01-24 22:45:52 -0500

    fixed issues with inverse kinematics

--- src/arm.js
+++ src/arm.js
@@ -17,6 +17,8 @@ process.__defineGetter__("stdin", function() {
 	return process.__stdin;
 });
 
+var last;
+
 var Arm = function(board) {
 	this.board = board;
 
@@ -36,7 +38,7 @@ var Arm = function(board) {
 		var pin = servoPins[name];
 		this.servos[name] = new five.Servo(pin);
 	}
-	console.log(this.servos);
+	// console.log(this.servos);
 	this.lights = new five.Led(ledsPin);
 	this.forearmLength = 128.86;
 	this.upperArmLength = 140;
@@ -60,8 +62,8 @@ Arm.prototype = (function() {
 			up: 0
 		},
 		'elbow': { // has to be given values in this range -- otherwise sevo disengages
-			down: 23,
-			up: 157
+			down: 25,
+			up: 156
 		},
 		'shoulderLeft': {
 			back: 0,
@@ -77,15 +79,32 @@ Arm.prototype = (function() {
 		}
 	}
 
-	// could probably do away with some of these:
-
-	// arm.setServo = function(servoName, value) {
-	// 	this.servos[servoName].to(value);
-	// }
-
 	arm.setPinch = function(value) { // 0 - open, 1- closed
 		var bounds = servoBounds['pinch'];
 		this.servos['pinch'].to(scale(value, bounds.open, bounds.closed));
+	}
+
+	// consider changing to take angle
+	arm.setRoll = function(value) { // 0 - facing left, 1 - right
+		var bounds = servoBounds['roll'];
+		this.servos['roll'].to(scale(value, bounds.left, bounds.right));
+	}
+
+	arm.setPitch = function(value) { // 0 - down, 1 - up
+		var bounds = servoBounds['pitch'];
+		this.servos['pitch'].to(scale(value, bounds.down, bounds.up));
+	}
+
+	arm.setElbow = function(value) { // 0.0 (down)- 1.0 (up)
+		var bounds = servoBounds['elbow'];
+		this.servos['elbow'].to(scale(value, bounds.down, bounds.up));	
+	} 
+
+	arm.setShoulder = function(value) { // 0.0 (back) - 1.0 (forward) 
+		var leftBounds = servoBounds['shoulderLeft'];
+		var rightBounds = servoBounds['shoulderRight'];
+		this.servos['shoulderLeft'].to(scale(value, leftBounds.back, leftBounds.forward));
+		this.servos['shoulderRight'].to(scale(value, rightBounds.back, rightBounds.forward));
 	}
 
 	arm.setBase = function(value) { // 0.0 (left) - 1.0 (right)
@@ -98,47 +117,61 @@ Arm.prototype = (function() {
 		// x: -1 (left), 1 (right)
 		// y: 0 (down), 1 (up)
 
-		var l1 = this.upperArmLength,
-		l2 = this.forearmLength;
+		var l1 = this.upperArmLength;
+		var l2 = this.forearmLength;
 
 		var base = this.calculateRotation(x, z);
-		var distance = dist(x, z) * (l1 + l2);
-
+		
 		y = y * (l1 + l2);
+		x = x * (l1 + l2);
+		z = z * (l1 + l2);
+
+		var distance = dist(x, z);
 
 		var theta_2 = Math.atan2(-Math.sqrt(1 - (Math.pow(Math.pow(distance, 2) + Math.pow(y, 2) - Math.pow(l1, 2) - Math.pow(l2, 2) / (2 * l1 * l2), 2)), ((Math.pow(distance, 2) + Math.pow(y, 2) - Math.pow(l1, 2) - Math.pow(l2, 2)) / (2 * l1 * l2)), 2));
+
+		var a = Math.pow(distance, 2) + Math.pow(y, 2) - Math.pow(l1, 2) - Math.pow(l2, 2); 
+		var b = (2 * l1 * l2);
+		theta_2 = Math.atan2(-Math.sqrt(1 - Math.pow(a / b, 2)), (a / b));
+
 		var k1 = l1 + l2 * Math.cos(theta_2);
 		var k2 = l2 * Math.sin(theta_2);
 
-		var theta_1 = Math.atan2(y, distance) - Math.atan2(k2, k1)  // in radians
-		var shoulder = radToDeg(theta_1) / 180;
-		var elbow = radToDeg(theta_2) / 180;
+		var theta_1 = Math.atan2(y, distance) - Math.atan2(k2, k1);  // in radians
+		var shoulder = radToDeg(theta_1);
+		var elbow = radToDeg(theta_2);
+
+		if (!isNaN(shoulder)) {
+
+			last = elbow;
 
 		this.setBase(base / 180); // maybe later change these to just get angle and set it
-		// this.setShoulder(shoulder);
-		// this.setElbow(elbow);
-
-		console.log(shoulder, elbow);
-
-
+		this.setShoulder(shoulder / 180);
+		this.setElbow((180 + elbow) / 180);
 
 	}
-
-	arm.calculateRotation = function(x, z) {
-		var rotation = radToDeg(Math.atan(z / x));
-		if (x < 0) {
-		 	rotation = 180 + rotation;
-		}
-		return 180 - rotation;
+	else {
+		this.setElbow( (180 + last) / 180 );
 	}
 
-	function dist(a, b) {
-		return Math.sqrt(Math.pow(a, 2), Math.pow(b, 2));
-	}
+	console.log(shoulder, elbow);
+}
 
-	function radToDeg(radians) {
-		return radians * (180 / Math.PI);
+arm.calculateRotation = function(x, z) {
+	var rotation = radToDeg(Math.atan(z / x));
+	if (x < 0) {
+		rotation = 180 + rotation;
 	}
+	return 180 - rotation;
+}
+
+function dist(a, b) {
+	return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+}
+
+function radToDeg(radians) {
+	return radians * (180 / Math.PI);
+}
 
 	function scale(value, min, max) { // value from 0.0 - 1.0
 		return clamp(value, 0, 1) * (max - min) + min;
@@ -168,12 +201,14 @@ board.on('ready', function() {
 			
 			var pinch = hand.pinchStrength;
 
-			var roll = radToDeg(hand.roll());
-			var pitch = radToDeg(hand.pitch());
+			var roll = radToDeg(hand.roll()); // -90: facing left, 90: facing right
+			var pitch = radToDeg(hand.pitch()); // -90: down, 90: up
 
 			arm.setPinch(pinch);
-			// arm.to(pos.x, pos.y, pos.z);
-			debug(pinch);
+			arm.setRoll( (roll + 90) / 180 );
+			arm.setPitch( (pitch + 90) / 180);
+			arm.to(pos.x, pos.y, pos.z);
+			debug(roll);
 			document.getElementById('coords').innerHTML = '('+String(pos.x)+', '+String(pos.y)+', '+String(pos.z)+')';
 		}
 	});
